df35c0b1071c850a2c115f763126060008c0a9f8
ckenza <kenza.chrifi36@gmail.com>
2024-11-28 23:19:38 +0100

    exo variables

--- exo.js
+++ exo.js
@@ -1,39 +1,7 @@
-// exo 1
-console.log("J'aime peindre");
-console.log("J'aimerais devenir peintre");
-
-// exo 2
-console.log("J'aime peindre.", "J'aimerais devenir peintre");
-
-// exo 3
-console.warn("Mais cela demande beaucoup d'efforts");
-
-// exo 4
-console.error("Je n'ai pas assez de temps car j'ai déjà un métier, 3 passions, une femme et 4 enfants et je dois dormir la nuit")
-
-// exo 5
-// Voici un jeu de 10 données. Ne pas y toucher :) 
-const number1 = 99; 
-const number2 = 50; 
-const number3 = 1;
-const number4 = 7; 
-const number5 = 3;
-const number6 = 10; 
-const number7 = 8;
-const number8 = 2; 
-const number9 = 1;
-const number10 = 4; 
-
-const set1 = number2 - number7
-const set2 = (number1 / number5) + (number4 + number3 + number9)
-const set3 = (number6 *number10) + number8
-
-console.log(set1);
-console.log(set2);
-console.log(set3);
-
-
-// exo 6
-console.log(6 % 2)
-console.log(14 % 4)
-console.log(99 % 3)
+const firstName = "Bruno";
+const introduction = "Salut je m'appelle Bruno et voici ma description";
+const job = "Analyste financier au Yémen";
+let age = 23;
+const city = "Bordeaux";
+const arrayNumber = "Une liste de nombres";
+const agentName = "le prénom du plus grand agent secret de tous les temps";
