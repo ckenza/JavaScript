b0ab53e59774c1780271157867204ca180679286
ckenza <kenza.chrifi36@gmail.com>
2024-11-28 23:28:11 +0100

    exo type de donnes

--- exo.js
+++ exo.js
@@ -1,7 +1,33 @@
-const firstName = "Bruno";
-const introduction = "Salut je m'appelle Bruno et voici ma description";
-const job = "Analyste financier au Yémen";
-let age = 23;
-const city = "Bordeaux";
-const arrayNumber = "Une liste de nombres";
-const agentName = "le prénom du plus grand agent secret de tous les temps";
+const firstname = "Michel";
+const lastname = "Polnareff";
+let age2 = 123;
+const isUserLoggedIn = false;
+const account = undefined;
+let account2;
+const myVar = null;
+const user = {
+    firstname: "Michel",
+    lastname: "Polnareff",
+    email: "mich-mich@pol.com"
+}
+const colors = ['red', 'blue', 'green'];
+const onSayHello = function sayHello() {
+    return 'Hello';
+}
+
+console.log(typeof "Michel");
+console.log(typeof "Polnareff");
+console.log(typeof 123);
+console.log(typeof false);
+console.log(typeof undefined);
+console.log(typeof account2);
+console.log(typeof null);
+console.log(typeof {
+    firstname: "Michel",
+    lastname: "Polnareff",
+    email: "mich-mich@pol.com"
+});
+console.log(typeof ['red', 'blue', 'green']);
+console.log(typeof function sayHello() {
+    return 'Hello';
+});
